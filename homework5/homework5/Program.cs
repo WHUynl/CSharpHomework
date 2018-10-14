cea6a6b2e2b1ae28c0ad7848c8ed2db82e43a2da
念麟 余 <yunianlin@outlook.com>
2018-10-14 21:46:21 +0800

    第五次作业

---
+++ homework5/homework5/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    class Program
+    {
+        class MyException : ApplicationException
+        {
+            private string idnumber = "输入类型错误或输入数值不正确";
+            public MyException()
+            {
+
+            }
+            public void outputerror()
+            {
+                Console.WriteLine(idnumber);
+            }
+
+        }
+        public class Order : OrderDetails
+        {
+            public Order(string a, double b, double c, string d, string f)
+            {
+                thing = a;
+                amount = b;
+                price = c;
+                name = d;
+
+                number = f;
+            }
+
+
+        }
+        public class OrderDetails
+        {
+            public string thing;
+            public double amount;
+            public double price;
+            public string name;
+
+            public string number;
+        }
+        public class OrderService
+        {
+
+            public List<Order> orders = new List<Order>();
+            public List<int> query = new List<int>();
+
+            public void setList(Order a)
+            {
+                orders.Add(a);
+            }
+            public void showList(int i)
+            {
+                Console.WriteLine("商品名称，数量，价格，买家姓名，订单号分别为： " + orders[i].thing + " " + orders[i].amount + " " + orders[i].price + " " + orders[i].name + " " + orders[i].number);
+            }
+
+            public void removeList(int i)
+            {
+                try
+                {
+                    removeOrder(i);
+                }
+                catch (MyException e) { e.outputerror(); }
+            }
+            public void removeOrder(int i)
+            {
+                if (i >= orders.Count || i < 0)
+                {
+                    throw new MyException();
+                }
+
+
+                orders.RemoveAt(i);
+
+
+            }
+
+            public void changeOrdername(int i, string a)
+            {
+                orders[i].name = a;
+            }
+            public void changeOrdernumber(int i, string a)
+            {
+                orders[i].number = a;
+            }
+            public void changeOrderthing(int i, string a)
+            {
+                orders[i].thing = a;
+            }
+            public void changeOrderamount(int i, double a)
+            {
+                orders[i].amount = a;
+            }
+            public void changeOrderprice(int i, double a)
+            {
+                orders[i].price = a;
+            }
+            public void findOrder(string a)
+
+            {
+                int i = -1;
+                query.Clear();
+                for (int k = 0; k < orders.Count; k++)
+                {
+                    query.Add(k);
+                }
+                var m = from item in query where orders[item].name.Equals(a) || orders[item].thing.Equals(a) select item;
+                foreach (var n in m)
+                {
+                    i = 1;
+                    Console.WriteLine("the location is : " + n);
+                }
+                if (i == -1)
+                {
+                    Console.WriteLine("Nothing");
+                }
+            }
+            public void findMoney(int a)
+
+            {
+                int i = -1;
+                query.Clear();
+                for (int k = 0; k < orders.Count; k++)
+                {
+                    query.Add(k);
+                }
+                var m = from item in query where orders[item].price > a select item;
+                foreach (var n in m)
+                {
+                    i = 1;
+                    Console.WriteLine("the location is : " + n);
+                }
+                if (i == -1)
+                {
+                    Console.WriteLine("Nothing");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            OrderService one = new OrderService();
+            Order first = new Order("bread", 100, 10000, "yunianlin", "001");//参数依次为：商品名称，数量，价格，买家姓名，订单号
+            Order second = new Order("milk", 10, 100000, "yu", "030");
+            Order third = new Order("cake", 20, 200000, "wang", "059");
+
+            one.setList(first);//添加订单
+            one.setList(second);
+            one.setList(third);
+            one.showList(1);//输入位置值，展示订单
+            one.findOrder("yunianlin");//查询订单,输入买家名得到位置值
+            one.findOrder("milk");//查询订单，输入商品名得到位置值
+            one.findMoney(10000);//查询订单，输入价格，得到大于该价格的订单的位置值
+
+
+        }
+    }
+}
---
+++ homework5/topic2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace topic2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (graphics == null) graphics = this.CreateGraphics();
+            output();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+
+        }
+        private Graphics graphics;
+
+        double th1;
+        double th2;
+        double per1;
+        double per2;
+        double k;
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double x2 = x0 + leng * k * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double y2 = y0 + leng * k * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            try
+            {
+                graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+            catch (Exception e) { Console.WriteLine("input error: " + e.Message); }
+        }
+        void output()
+        {
+            try
+            {
+                string a = textBox1.Text;
+                string b = textBox2.Text;
+                string c = textBox3.Text;
+                string d = textBox4.Text;
+                string e = textBox5.Text;
+
+                double A = double.Parse(a);
+                double B = double.Parse(b);
+                double C = double.Parse(c);
+                double D = double.Parse(d);
+                double E = double.Parse(e);
+
+                k = E;
+                th1 = A * Math.PI / 180;
+                th2 = B * Math.PI / 180;
+                per1 = C;
+                per2 = D;
+            }
+            catch (Exception e) { Console.WriteLine("input error: " + e.Message); }
+        }
+
+
+    }
+}
